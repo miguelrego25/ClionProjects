ee799df71a99b1a43e05e81842d8d8ee9b9fd712
miguelrego25 <61994815+miguelrego25@users.noreply.github.com>
2023-02-17 10:37:38 +0000

    Add files via upload
    Atualizacao de pasta

---
+++ Sistemas Operativos 2022/Aula3/cmake-build-debug/CMakeFiles/Aula3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Aula3.exe"
+  "Aula3.exe.manifest"
+  "Aula3.pdb"
+  "CMakeFiles/Aula3.dir/main.c.obj"
+  "libAula3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Aula3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
