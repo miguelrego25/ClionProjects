ffd2193838e49a3fcefa4f1b68ae5d4411544911
miguelrego25 <miguel.m.rego@gmail.com>
2023-02-23 11:27:10 +0000

    Savecp

--- Sistemas Operativos 2023/Homework/Guiao1/main.c
+++ Sistemas Operativos 2023/Homework/Guiao1/main.c
@@ -1,7 +1,34 @@
 //Resolução do guião 1
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#define MAX 1024
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+int mycp(char* input_file, char* output_file, int size){
+    int fd_in = open(input_file, O_RDONLY);
+    if (fd_in < 0){
+        perror("Erro ao abrir o ficheiro de entrada");
+        return -1;
+    }
+    int fd_out = open(output_file, O_RDWR);
+    if(fd_out < 0){
+        perror("Erro ao abrir o ficheiro de entrada");
+        return -1;
+    }
+
+    char buffer[MAX];
+    ssize_t bytes_read = read(fd_in, buffer,size);
+
+    ssize_t bytes_written = write(fd_out,buffer,size);
+
+    close(fd_in);
+    close(fd_out);
+
+}
+
+
+int main(int argc, char** argv) {
+    mycp(argv[1], argv[2],argc);
 }
