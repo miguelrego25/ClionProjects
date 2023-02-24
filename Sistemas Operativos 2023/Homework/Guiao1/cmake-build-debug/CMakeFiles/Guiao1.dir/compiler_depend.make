345bed2ced0435cc00e522f0ca4b9e44bed31c95
miguelrego25 <miguel.m.rego@gmail.com>
2023-02-24 11:11:34 +0000

    Not working still

--- Sistemas Operativos 2023/Homework/Guiao1/cmake-build-debug/CMakeFiles/Guiao1.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for Guiao1.
-# This may be replaced when dependencies are built.
--- Sistemas Operativos 2023/Homework/Guiao1/main.c
+++ Sistemas Operativos 2023/Homework/Guiao1/main.c
@@ -7,26 +7,25 @@
 #define MAX 1024
 
 int mycp(char* input_file, char* output_file, int size){
-    int fd_in = open(input_file, O_RDONLY);
+    int fd_in = open(input_file,O_RDONLY);
     if (fd_in < 0){
-        perror("Erro ao abrir o ficheiro de entrada");
+        perror("invalid input file");
         return -1;
     }
-    int fd_out = open(output_file, O_RDWR);
-    if(fd_out < 0){
-        perror("Erro ao abrir o ficheiro de entrada");
+    int fd_out = open(output_file,O_WRONLY);
+    if (fd_out < 0){
+        perror("invalid output file");
         return -1;
     }
 
-    char buffer[MAX];
-    ssize_t bytes_read = read(fd_in, buffer,size);
-
-    ssize_t bytes_written = write(fd_out,buffer,size);
+    char buff[MAX];
+    ssize_t byten_read = read(fd_in,buff,size);
+    ssize_t byten_written = write(fd_out, buff, size);
 
     close(fd_in);
     close(fd_out);
-
 }
+
 
 
 int main(int argc, char** argv) {
