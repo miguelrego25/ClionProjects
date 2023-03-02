4b3b2e18958521b7dc2c372165e9a56381c092d5
miguelrego25 <miguel.m.rego@gmail.com>
2023-03-02 12:46:50 +0000

    cilindro not working so fiz o fundo e o topo falta fazer os lados

---
+++ ComputaçãoGráfica/Aulas/2ªaula/main.cpp
@@ -0,0 +1,155 @@
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+void changeSize(int w, int h) {
+
+	// Prevent a divide by zero, when window is too short
+	// (you cant make a window with zero width).
+	if(h == 0)
+		h = 1;
+
+	// compute window's aspect ratio 
+	float ratio = w * 1.0 / h;
+
+	// Set the projection matrix as current
+	glMatrixMode(GL_PROJECTION);
+	// Load Identity Matrix
+	glLoadIdentity();
+	
+	// Set the viewport to be the entire window
+    glViewport(0, 0, w, h);
+
+	// Set perspective
+	gluPerspective(45.0f ,ratio, 1.0f ,1000.0f);
+
+	// return to the model view matrix mode
+	glMatrixMode(GL_MODELVIEW);
+}
+
+void drawAxis(){ // put axis drawing in here
+    glBegin(GL_LINES);
+// X Axis in red
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex3f(
+            -100.0f, 0.0f, 0.0f);
+    glVertex3f( 100.0f, 0.0f, 0.0f);
+// Y Axis in Green
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glVertex3f(0.0f,
+               -100.0f, 0.0f);
+    glVertex3f(0.0f, 100.0f, 0.0f);
+// Z Axis in Blue
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(0.0f, 0.0f,
+               -100.0f);
+    glVertex3f(0.0f, 0.0f, 100.0f);
+    glEnd();
+
+}
+
+void drawCylinder(float radius, float height, int slices) {
+    float radiustop = radius;
+    float radiusbottom = radius;
+    drawAxis();
+
+    glBegin(GL_TRIANGLE_FAN);
+
+    //vertice de cima
+    glVertex3f(0.0,0.5,0.0);
+
+    //Construção do topo do cilindro
+    //Para torna lo mais redondo basta reduzir a incrementação do radius dentro do circulo
+    float startingangle = radius + 2 * M_PI;
+
+    while(radiustop < startingangle && slices){
+        glVertex3f(sin(radiustop),height/2,cos(radiustop));
+        radiustop = radiustop + M_PI/9;
+    }
+    glEnd();
+
+    glBegin(GL_TRIANGLE_FAN);
+
+    //vertice de baixo
+    glVertex3f(0.0,-0.5,0.0);\\
+
+    //contrução do Fundo do cilindro
+
+    while(radiusbottom < startingangle && slices){
+        glVertex3f(sin(radiusbottom),-height/2,cos(radiusbottom));
+        radiusbottom = radiusbottom + M_PI/9;
+    }
+    glEnd();
+
+
+
+
+
+
+
+}
+
+
+void renderScene(void) {
+
+	// clear buffers
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// set the camera
+	glLoadIdentity();
+	gluLookAt(5.0,5.0,5.0, 
+		      0.0,0.0,0.0,
+			  0.0f,1.0f,0.0f);
+
+	drawCylinder(1,2,10);
+
+	// End of frame
+	glutSwapBuffers();
+}
+
+
+void processKeys(unsigned char c, int xx, int yy) {
+
+// put code to process regular keys in here
+
+}
+
+
+void processSpecialKeys(int key, int xx, int yy) {
+
+// put code to process special keys in here
+
+}
+
+
+int main(int argc, char **argv) {
+
+// init GLUT and the window
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH|GLUT_DOUBLE|GLUT_RGBA);
+	glutInitWindowPosition(100,100);
+	glutInitWindowSize(800,800);
+	glutCreateWindow("CG@DI-UM");
+		
+// Required callback registry 
+	glutDisplayFunc(renderScene);
+	glutReshapeFunc(changeSize);
+	
+// Callback registration for keyboard processing
+	glutKeyboardFunc(processKeys);
+	glutSpecialFunc(processSpecialKeys);
+
+//  OpenGL settings
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+	
+// enter GLUT's main cycle
+	glutMainLoop();
+	
+	return 1;
+}
